4de88730e798a7f821904613dccc6191050c6032
tangjicheng@ec2 <tang.j@ctw.inc>
2023-05-12 05:38:15 +0000

    modified:   startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,5 @@
+wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-2022.05-Linux-x86_64.sh
+
 conda create -y -n sd_trt python=3.10
 conda activate sd_trt
 pip install -r requirements.txt
